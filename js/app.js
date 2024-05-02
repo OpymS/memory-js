ebf8ee0f63bcda9d5aa61ffeab3492d71f0c40df
OpymS <maisonneuve.menuisier@gmail.com>
2024-05-02 07:52:40 +0200

    IT1-ID40 animation cartes => gestion des délais

--- js/app.js
+++ js/app.js
@@ -1,9 +1,15 @@
+const NBR_FRAMES = 6
+const INTERVAL = 5
+
 let nbrCols = 4;
 let nbrLines = 3;
 let flippedCards = 0;
+let tmpFlippedCards = 0;
 let firstCard, secondCard, yFirstCard, xFirstCard;
 let score = 0;
 let board;
+let cardsBack = new Image();
+cardsBack.src = "/ressources/question.svg";
 
 const deck = "/ressources/memory-legume";
 
@@ -11,14 +17,16 @@ const $canvas = document.getElementById("myCanvas");
 const ctx = $canvas.getContext("2d");
 
 const $score = document.querySelector('.score')
-console.log($score.textContent);
 const width = $canvas.width;
 const height = $canvas.height;
 const dimSquare = Math.floor(Math.min(width / nbrCols, height / nbrLines));
 
 board = initBoard(nbrLines, nbrCols);
 board = shuffle(board, deck);
-drawBoard(board);
+
+cardsBack.addEventListener("load", () => {
+  drawBoard(board);
+})
 
 document.addEventListener('keydown', (event) => {
   if (event.code == 'Space') {
@@ -36,38 +44,48 @@ $canvas.addEventListener("mousedown", function (event) {
   const x = Math.floor((event.clientX - rect.left) / dimSquare);
   const y = Math.floor((event.clientY - rect.top) / dimSquare);
   let pair = false;
-  if (board[y][x][1] == 0) {
+  if (tmpFlippedCards < 2 && board[y][x][1] == 0) {
+    tmpFlippedCards++
     board[y][x][1] = 1;
     flipCard(y, x);
     flippedCards++;
-    if (flippedCards % 2 == 0) {
+    if (flippedCards % 2 == 1) {
+      firstCard = board[y][x][0];
+      xFirstCard = x;
+      yFirstCard = y;
+    } else {
       score++;
       $score.textContent = score
       secondCard = board[y][x][0];
       pair = checkPair(firstCard, secondCard);
-    } else {
-      firstCard = board[y][x][0];
-      xFirstCard = x;
-      yFirstCard = y;
     }
   }
-  if (!pair && flippedCards % 2 == 0) {
-    flippedCards -= 2;
-    board[y][x][1] = 0;
-    board[yFirstCard][xFirstCard][1] = 0;
-    setTimeout(() => {
-      flipBackCard(y, x);
-      flipBackCard(yFirstCard, xFirstCard);
-    }, 500);
+  if (flippedCards % 2 == 0) {
+    if (pair) {
+      setTimeout(() => {
+        tmpFlippedCards = 0
+      }, (NBR_FRAMES + 1) * INTERVAL + 500);
+    } else {
+      flippedCards -= 2;
+      board[y][x][1] = 0;
+      board[yFirstCard][xFirstCard][1] = 0;
+      setTimeout(() => {
+        flipBackCard(y, x);
+        flipBackCard(yFirstCard, xFirstCard);
+        tmpFlippedCards = 0
+      }, (NBR_FRAMES + 1) * INTERVAL + 500);
+    }
+
   }
   if (flippedCards == nbrCols * nbrLines) {
     ctx.font = "bold 72px Arial";
-    ctx.fillStyle = "purple";
     setTimeout(() => {
+      ctx.fillStyle = "purple";
       ctx.fillText("Bravo !", 200, 150)
       ctx.fillText(`Vous avez gagné en ${score} coups`, 5, 300, 590);
-    }, 500);
+    }, (NBR_FRAMES + 1) * INTERVAL + 500);
   }
+
 });
 
 function initBoard(nbLines, nbCols) {
@@ -94,7 +112,6 @@ function shuffle(board, deck) {
         if (board[line][col][1] == -1) {
           board[line][col][1] = 0;
           board[line][col][0] = i;
-          // board[line][col][0] = `${deck}/${i}.svg`;
           isPut = true;
         }
       } while (!isPut);
@@ -110,67 +127,22 @@ function drawBoard(board) {
   for (let i = 0; i < nbLines; i++) {
     for (let j = 0; j < nbCols; j++) {
       ctx.fillStyle = "white";
-      ctx.fillRect(
-        j * dimSquare + 3,
-        i * dimSquare + 3,
-        dimSquare - 6,
-        dimSquare - 6
-      );
-      const image = new Image();
-      image.addEventListener("load", () => {
-        ctx.drawImage(
-          image,
-          j * dimSquare + 15,
-          i * dimSquare + 15,
-          dimSquare - 30,
-          dimSquare - 30
-        );
-      });
-      image.src = "/ressources/question.svg";
+      ctx.fillRect(j * dimSquare + 3, i * dimSquare + 3, dimSquare - 6, dimSquare - 6);
+      ctx.drawImage(cardsBack, j * dimSquare + 15, i * dimSquare + 15, dimSquare - 30, dimSquare - 30);
     }
   }
 }
 
 function flipCard(i, j) {
-  ctx.fillStyle = "white";
-  ctx.fillRect(
-    j * dimSquare + 3,
-    i * dimSquare + 3,
-    dimSquare - 6,
-    dimSquare - 6
-  );
-  const image = new Image();
-  image.addEventListener("load", () => {
-    ctx.drawImage(
-      image,
-      j * dimSquare + 15,
-      i * dimSquare + 15,
-      dimSquare - 30,
-      dimSquare - 30
-    );
+  const endImage = new Image();
+  endImage.src = `${deck}/${board[i][j][0]}.svg`;
+  endImage.addEventListener("load", () => {
+    animateCard(i, j, endImage)
   });
-  image.src = `${deck}/${board[i][j][0]}.svg`;
 }
 
 function flipBackCard(i, j) {
-  ctx.fillStyle = "white";
-  ctx.fillRect(
-    j * dimSquare + 3,
-    i * dimSquare + 3,
-    dimSquare - 6,
-    dimSquare - 6
-  );
-  const image = new Image();
-  image.addEventListener("load", () => {
-    ctx.drawImage(
-      image,
-      j * dimSquare + 15,
-      i * dimSquare + 15,
-      dimSquare - 30,
-      dimSquare - 30
-    );
-  });
-  image.src = "/ressources/question.svg";
+  animateCard(i, j, cardsBack)
 }
 
 function checkPair(firstCard, secondCard) {
@@ -179,4 +151,44 @@ function checkPair(firstCard, secondCard) {
   } else {
     return false;
   }
+}
+
+
+function animateCard(y, x, endImage) {
+  let i = 0
+  setTimeout(() => {
+    ctx.clearRect(x * dimSquare - 3, y * dimSquare - 3, dimSquare + 6, dimSquare + 6);
+    ctx.fillRect((x * dimSquare + 3), y * dimSquare + 3, (dimSquare - 6), dimSquare - 6)
+    ctx.drawImage(endImage, (x * dimSquare + 15), y * dimSquare + 15, (dimSquare - 30), dimSquare - 30);
+  }, (NBR_FRAMES + 1) * INTERVAL);
+  const interval = setInterval(() => {
+    ctx.clearRect(x * dimSquare - 3, y * dimSquare - 3, dimSquare + 6, dimSquare + 6);
+    ctx.fillStyle = "white";
+    if (i < NBR_FRAMES / 2) {
+      const horizDecalage = (dimSquare - 6) * (i + 1) / NBR_FRAMES
+      const vertDecalage = dimSquare / 3 * (i + 1) / NBR_FRAMES
+      ctx.beginPath();
+      ctx.moveTo(x * dimSquare + 3 + horizDecalage, y * dimSquare + 3 + vertDecalage);
+      ctx.lineTo((x + 1) * dimSquare - 3 - horizDecalage, y * dimSquare + 3 - vertDecalage);
+      ctx.lineTo((x + 1) * dimSquare - 3 - horizDecalage, (y + 1) * dimSquare - 3 + vertDecalage);
+      ctx.lineTo(x * dimSquare + 3 + horizDecalage, (y + 1) * dimSquare - 3 - vertDecalage);
+      ctx.lineTo(x * dimSquare + 3 + horizDecalage, y * dimSquare + 3 + vertDecalage)
+      ctx.fill();
+    } else {
+      const horizDecalage = (dimSquare - 6) * (NBR_FRAMES - i - 1) / NBR_FRAMES
+      const vertDecalage = -(dimSquare / 3 * (NBR_FRAMES - i - 1) / NBR_FRAMES)
+      ctx.beginPath();
+      ctx.moveTo(x * dimSquare + 3 + horizDecalage, y * dimSquare + 3 - vertDecalage);
+      ctx.lineTo((x + 1) * dimSquare - 3 - horizDecalage, y * dimSquare + 3 + vertDecalage);
+      ctx.lineTo((x + 1) * dimSquare - 3 - horizDecalage, (y + 1) * dimSquare - 3 - vertDecalage);
+      ctx.lineTo(x * dimSquare + 3 + horizDecalage, (y + 1) * dimSquare - 3 + vertDecalage);
+      ctx.lineTo(x * dimSquare + 3 + horizDecalage, y * dimSquare + 3 - vertDecalage)
+      ctx.fill();
+    }
+    i++
+    if (i >= NBR_FRAMES) {
+      clearInterval(interval)
+    }
+  }, INTERVAL);
+
 }
